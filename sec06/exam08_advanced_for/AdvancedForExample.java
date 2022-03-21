af06aeff556789583e447b72abc8d7bcb01da74c
KIM0212DH <93184884+KIM0212DH@users.noreply.github.com>
2022-03-21 15:03:11 +0900

    Add files via upload

---
+++ sec06/exam08_advanced_for/AdvancedForExample.java
@@ -0,0 +1,18 @@
+package sec06.exam08_advanced_for;
+
+public class AdvancedForExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = { 95, 71, 84, 93, 87 };
+		
+		int sum = 0;
+		for (int score : scores)
+			sum = score + sum;
+		System.out.println("Á¡¼ö ÃÑÇÕ = " + sum);
+		
+		double avg = (double) sum / scores.length;
+		System.out.println("Á¡¼ö Æò±Õ = " + avg);
+	}
+
+}
---
+++ sec07/exam01_enum/EnumMethodExample.java
@@ -0,0 +1,50 @@
+package sec07.exam01_enum;
+
+public class EnumMethodExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//name method
+		Week today = Week.SUNDAY;
+		String name = today.name();
+		System.out.println(name);
+		
+		//ordinal method
+		int ordinal = today.ordinal();
+		System.out.println(ordinal);
+		
+		//compareTo method
+		Week day1 = Week.MONDAY;
+		Week day2 = Week.WEDNESDAY;
+		int result1 = day1.compareTo(day2);
+		int result2 = day2.compareTo(day1);
+		System.out.println(result1);
+		System.out.println(result2);
+		
+		//valueOf method
+		/*Week weekDay = Week.valueOf("SUNDAY");
+		 * if(weekDay == Week.SATURDAY || weekDay == Week.SUNDAY)
+		 * {
+		 * 	System.out.println("林富");
+		 * }
+		 * else{
+		 * 	System.out.println("乞老");
+		 * }
+		 */
+		
+		if (args.length == 1) {
+			String strDay = args[0];
+			Week weekDay = Week.valueOf(strDay);
+			if (weekDay == Week.SATURDAY || weekDay == Week.SUNDAY)
+				System.out.println("林富");
+			else
+				System.out.println("乞老");
+		}
+		
+		//values method
+		Week[] days = Week.values();
+		for (Week day : days)
+			System.out.println(day);
+	}
+
+}
